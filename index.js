50311298b07d61d8e0f837c1168fc13bc5910ca9
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-26 11:19:49 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+function cats() {
+    return [ 'Milo','Otis,'Garfield']
+    }
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+ function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+destructivelyPrependCat("Bob");
+ 
+ function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+destructivelyRemoveLastCat();
+
+
+  function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+destructivelyRemoveFirstCat();
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+appendCat("Broom");
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+prependCat("Arnold");
+
+
